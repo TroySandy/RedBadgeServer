2b228e3b5bd8ba0746053fd5d6506f19335f4c2c
TroySandy <troysandy16@gmail.com>
2021-05-12 14:39:09 -0400

    db association complete/ post endpoints working

--- controllers/MediaController.js
+++ controllers/MediaController.js
@@ -2,3 +2,45 @@ const router = require("express").Router();
 const { validateJWT } = require("../middleware");
 const { Media } = require("../models");
 const jwt = require("jsonwebtoken");
+
+
+router.get("/practice", (req, res) => {
+    res.send("Hey!! This is a practice route!!");
+  });
+
+router.post("/", validateJWT, async (req, res) => {
+    const {
+      media,
+      title,
+      private,
+    } = req.body;
+
+    try {
+      const newMedia = await Media.create({
+        media,
+        title,
+        private,
+        userId: req.user.id
+      });
+      res.status(201).json({
+        message: "File Uploaded",
+        newMedia,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: `Failed to upload file ${err}`,
+      });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+  module.exports = router;
--- models/CommentsModel.js
+++ models/CommentsModel.js
@@ -2,9 +2,19 @@ const db = require("../db");
 const { DataTypes } = require("sequelize");
 
 const Comments = db.define("comments", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+  },
   comment: {
     type: DataTypes.STRING,
     allowNull: false,
+  },
+  heading: {
+    type: DataTypes.STRING,
+    allowNull: true
   },
   rating: {
     type: DataTypes.DECIMAL,
