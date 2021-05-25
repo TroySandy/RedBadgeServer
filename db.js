5f3d180ceaeb51aff0a90d8a782a5da69c0ef3d5
TroySandy <troysandy16@gmail.com>
2021-05-25 15:15:04 -0400

    deploy

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 const Sequelize = require("sequelize").Sequelize;
 
-let sequelize = new Sequelize(process.env.DB_CONN_URL);
+let sequelize = new Sequelize(process.env.DB_CONN_URL, {
+  dialect: "postgres",
+  ssl: process.env.ENVIRONMENT === "production",
+});
 
 module.exports = sequelize;
